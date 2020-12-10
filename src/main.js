b2346ba908a1e435bf446896fe80a438d97f7786
ocarrillor <ocarrillo7@ucol.mx>
2020-12-10 14:09:27 -0600

    added mising test files

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
-export default class App {}
+export default class App {
+    sumatoriaSerieUno(numero){
+        let suma;
+        for(let i = 1; i <= numero; i++){
+            console.log(i + 1);
+        }
+    }
+}
 
 let app = new App();
+
+//Demostracion de la funcion sumatoriaSerieUno.
+console.log(app.sumatoriaSerieUno(20));

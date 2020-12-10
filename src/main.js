4ee384644e9188e354f42691c1ee8d03d05a1aa4
ocarrillor <ocarrillo7@ucol.mx>
2020-12-10 15:33:18 -0600

    Implemented function sumatoriaSerieDos

--- src/main.js
+++ src/main.js
@@ -7,9 +7,25 @@ export default class App {
         }
         return suma;
     }
+
+    sumatoriaSerieDos(numero){
+        let cont = 2, suma = 1;
+        while(cont <= numero){
+            if(cont % 2 == 0){
+                suma = suma + (1/cont);
+            }else{
+                suma = suma - (1/cont);
+            }
+            cont++;
+        }
+        return suma;
+    }
 }
 
 let app = new App();
 
 //Demostracion de la funcion sumatoriaSerieUno.
 console.log(app.sumatoriaSerieUno(20));
+
+//Demostracion de la funcion sumatoriaSerieUno.
+console.log(app.sumatoriaSerieDos(5));
